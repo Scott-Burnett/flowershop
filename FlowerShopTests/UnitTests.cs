5eb9dc9693e00227f251fc94a4f1f6c1acf5ac14
Scott-Burnett <thebreakfastfactoryyay@gmail.com>
2019-02-23 20:16:54 +0200

    Question 4: Using mocks to test

--- FlowerShopTests/UnitTests.cs
+++ FlowerShopTests/UnitTests.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using NSubstitute;
+using FlowerShop;
 
 namespace Tests
 {
@@ -12,7 +14,16 @@ namespace Tests
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            //ARRANGE
+            var orderDAO = Substitute.For<IOrderDAO>();
+            var client = Substitute.For<IClient>();
+            Order order = new Order(orderDAO, client);
+
+            //ACT
+            order.Deliver();
+
+            //ASSERT
+            orderDAO.Received().SetDelivered(order);
         }
     }
 }
